787701f64e43dc373ef4b61f905c9e4bd911f299
Pablo Zalem <zalem.pablo@yahoo.com>
2025-12-26 20:52:07 -0300

    Melhorando o codigo

--- src/main/java/Security/in/Java/Encryption/and/Decryption/AES.java
+++ src/main/java/Security/in/Java/Encryption/and/Decryption/AES.java
@@ -1,17 +1,12 @@
 package Security.in.Java.Encryption.and.Decryption;
 
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Possible KEY_SIZE values are 128, 192 and 256
@@ -19,42 +14,49 @@ import javax.crypto.spec.GCMParameterSpec;
  */
 
 public class AES {
-
-    // Create Secret Key
-    SecretKey secretKey;
+    private SecretKey key;
     private int KEY_SIZE = 128;
     private int T_LEN = 128;
     private byte[] IV;
+    private Cipher encryptionCipher;
 
-    public void init() throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(KEY_SIZE);
-        secretKey = keyGenerator.generateKey();
+    public void init() throws Exception {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(KEY_SIZE);
+        key = generator.generateKey();
     }
 
-    // Create Encryption mode
-    public String encrypt(String message) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-            IllegalBlockSizeException, BadPaddingException {
+    public void initFromStrings(String secretKey, String IV) {
+        key = new SecretKeySpec(decode(secretKey), "AES");
+        this.IV = decode(IV);
+    }
+
+    public String encryptOld(String message) throws Exception {
         byte[] messageInBytes = message.getBytes();
-        Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-        encryptionCipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        IV = encryptionCipher.getIV();
+        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
         byte[] encryptedBytes = encryptionCipher.doFinal(messageInBytes);
         return encode(encryptedBytes);
     }
 
-    // Create Decryption mode
-    public String decrypt(String encryptedMessage) throws NoSuchAlgorithmException, NoSuchPaddingException,
-            InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+    public String encrypt(String message) throws Exception {
+        byte[] messageInBytes = message.getBytes();
+        Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(T_LEN, IV);
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key, spec);
+        byte[] encryptedBytes = encryptionCipher.doFinal(messageInBytes);
+        return encode(encryptedBytes);
+    }
+
+    public String decrypt(String encryptedMessage) throws Exception {
         byte[] messageInBytes = decode(encryptedMessage);
         Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(T_LEN, IV);
-        decryptionCipher.init(Cipher.DECRYPT_MODE, secretKey, gcmParameterSpec);
+        GCMParameterSpec spec = new GCMParameterSpec(T_LEN, IV);
+        decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
         byte[] decryptedBytes = decryptionCipher.doFinal(messageInBytes);
         return new String(decryptedBytes);
     }
 
-    // Encode and Decode Base64
     private String encode(byte[] data) {
         return Base64.getEncoder().encodeToString(data);
     }
@@ -63,22 +65,16 @@ public class AES {
         return Base64.getDecoder().decode(data);
     }
 
-    private void exportKeys() {
-        System.err.println("Secret Key: " + encode(secretKey.getEncoded()));
-        System.err.println("IV: " + encode(IV));
-    }
-
     public static void main(String[] args) {
         try {
             AES aes = new AES();
-            aes.init();
-            String encryptionMessage = aes.encrypt("Hello World");
-            String decryptedMessage = aes.decrypt(encryptionMessage);
-            System.err.println("Encrypted Message: " + encryptionMessage);
-            System.err.println("Decrypted Message: " + decryptedMessage);
-            aes.exportKeys();
-        } catch (Exception e) {
+            aes.initFromStrings("CHuO1Fjd8YgJqTyapibFBQ==", "e3IYYJC2hxe24/EO");
+            String encryptedMessage = aes.encrypt("Example");
+            String decryptedMessage = aes.decrypt(encryptedMessage);
 
+            System.err.println("Encrypted Message : " + encryptedMessage);
+            System.err.println("Decrypted Message : " + decryptedMessage);
+        } catch (Exception ignored) {
         }
     }
 }
